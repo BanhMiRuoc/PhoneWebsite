09c2afe9bda442843a4ff11fde635cb65abe6713
city314 <biyeo126@gmail.com>
2024-05-17 14:08:05 +0700

    change html to php, create footer, add categories

--- phone/index.php
+++ phone/index.php
@@ -3,25 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- title web -->
+    <title>MeoPhones</title>
+    <!-- add font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Jockey+One&display=swap" rel="stylesheet">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="logo.png">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>MeoPhones</title>
     <link href="https://fonts.googleapis.com/css2?family=Jockey+One:wght@400&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/customer_management_1.css" />
-    <link rel="stylesheet" href="css/menu.css" />
+    <!-- add bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Add logo -->
+    <link rel="icon" type="image/x-icon" href="logo.png">
+    <!-- Add css -->
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/customer_management_1.css">
+    <link rel="stylesheet" href="css/footer.css">
 </head>
 <body>
     <div>
     <?php
-        include("pages/menu.php");
+        include("pages/header.php");
+        include("pages/cm1.php");
+        include("pages/footer.php");
     ?>
     </div>
 </body>
---
+++ phone/pages/footer.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+    <div class="container-fluid d-flex justify-content-center">
+        <div class="mt-3">
+            <p class="footer-content fw-bold">
+                Copyright
+                <img src="assets/images/image_7.png" alt="Logo Copyright" class="logo-copyright">
+                2024; Designed by MeoTeam
+            </p>
+        </div>
+    </div>
+</footer>

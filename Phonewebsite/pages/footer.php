2a93c0cf605f0137a7d6c6d81f19e057328485dd
Mai Trang <116065556+BanhMiRuoc@users.noreply.github.com>
2024-05-17 23:15:32 +0700

    create account for admin
    add new file php and css for create account

---
+++ Phonewebsite/pages/footer.php
@@ -0,0 +1,38 @@
+<footer class="footer">
+  <div class="row footer-social shadow-lg">
+    <div class="container-fluid d-flex justify-content-center my-3">
+      <!-- facebook -->
+      <a href="#">
+        <img src="assets/images/image_2.png" alt="logo Facebook" class="logo me-4">
+      </a>
+      <!-- youtube -->
+      <a href="#">
+        <img src="assets/images/image_3.png" alt="logo Youtube" class="logo me-4">
+      </a>
+      <!-- instagram -->
+      <a href="#">
+        <img src="assets/images/image_4.png" alt="logo Instagram" class="logo me-4">
+      </a>
+      <!-- tiktok -->
+      <a href="#">
+        <img src="assets/images/image_5.png" alt="logo Tiktok" class="logo me-4">
+      </a>
+      <!-- github -->
+      <a href="#">
+        <img src="assets/images/image_6.png" alt="logo Github" class="logo me-4">
+      </a>
+    </div>
+  </div>
+  <div class="row footer-copyright">
+    <div class="container-fluid content d-flex justify-content-center">
+      <div class="mt-3">
+        <p class="footer-content fw-bold">
+          Copyright
+          <img src="assets/images/image_7.png" alt="Logo Copyright" class="logo-copyright">
+          2024; Designed by MeoTeam
+        </p>
+      </div>
+    </div>
+  </div>
+</footer>
+<script href="js/main_no_register.js"></script>

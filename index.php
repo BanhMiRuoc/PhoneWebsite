b25936fe0e82ad8e6e8f2f8f8217b459c9aca9d9
city314 <biyeo126@gmail.com>
2024-05-19 19:13:13 +0700

    add report page

--- index.php
+++ index.php
@@ -21,6 +21,11 @@
     <script type="text/javascript" src="dist/js/jquery-1.10.2.js"></script>
     <link href="bootstrap-rating.css" rel="stylesheet">
     <script type="text/javascript" src="bootstrap-rating.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
     <!-- Add logo -->
     <link rel="icon" type="image/x-icon" href="logo.png">
     <!-- Add css -->

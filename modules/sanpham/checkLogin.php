a1850d3c9be57118e56de8a9855977aee56cae33
city314 <biyeo126@gmail.com>
2024-05-19 15:41:48 +0700

    add detaile_product in pages

--- modules/sanpham/checkLogin.php
+++ modules/sanpham/checkLogin.php
@@ -1,21 +1,38 @@
 <?php
-    include('config/connect.php');
+include('../../config/connect.php');
 
-    if(isset($_POST['btn-login'])) {
-        $username = $_POST['username'];
-        $password =$_POST['password'];
+if(isset($_POST['btn-login'])) {
+    $username = $_POST['username'];
+    $password =$_POST['password'];
 
-        $sql = "SELECT * FROM TAIKHOAN WHERE TENTK = '".$username."' AND MK= '".$password."'";
+    $sel_account = "SELECT COUNT(*) FROM TAIKHOAN WHERE TENTK = '$username' AND MK='$password'";
 
-        $row = mysqli_query($mysqli, $sql);
-        $count = mysqli_num_rows($row);
-        if ($count > 0) {
-            $_SESSION['dangnhap'] = $taikhoan;
-            header('Location:pages/index.php?user=login&action=admmin');
+
+    $result = $mysqli->query($sel_account);
+    $row = $result->fetch_row(); // Lấy kết quả từ truy vấn COUNT
+    $count = $row[0]; // Lấy giá trị COUNT
+
+    if ($count > 0) {
+
+        $sel_tt = "SELECT trangthai FROM TAIKHOAN WHERE TENTK = '$username'";
+        $result1 = $mysqli->query($sel_tt);
+        $row1 = $result1->fetch_row();
+        $trangthai = $row1[0];
+        if ($trangthai == "Chưa kích hoạt"){
+            echo "<script>alert('Vui lòng đăng nhập bằng cách nhấp vào liên kết trong email của bạn');</script>";
         }
-        else {
-            echo '<script>alert("Your usename or password is incorrect, please enter again.")</script>';
-            header('Location:../../index.php?user=login');
+        else{
+            header('Location:../../index.php?quanly=home&login=true');
+            exit(); // Dừng việc thực thi mã PHP ngay sau khi chuyển hướng
         }
     }
+    else {
+        echo "<script>alert('Tên đăng nhập hoặc mật khẩu không đúng');</script>";
+        echo "<script>setTimeout(function() { window.location.href = '../../index.php?quanly=login'; }, 1000);</script>";
+        exit(); // Dừng việc thực thi mã PHP ngay sau khi chuyển hướng
+    }
+}
+
+// Đóng kết nối
+$mysqli->close();
 ?>
